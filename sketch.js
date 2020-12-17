c3efb83ca9d562bb851457eefdf5b62ac29bb087
Manvitha15090 <75402486+Manvitha15090@users.noreply.github.com>
2020-12-17 16:42:47 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var fixedRect, movingRect, g1, g2, g3;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  g1 = createSprite(80,50,40,40);
+  g1.shapeColor = "green";
+  g2 = createSprite(150,50,40,40);
+  g2.shapeColor = "green";
+  g3 = createSprite(200,50,40,40);
+  g3.shapeColor = "green";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  if(isTouching(g1,movingRect)){
+    g1.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }
+  else {
+    g1.shapeColor = "green";
+    movingRect.shapeColor = "green";
+  }
+
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2
+      && object1.y - object2.y < object2.height/2 + object1.height/2
+      && object2.y - object1.y < object2.height/2 + object1.height/2) {
+        return true
+  }
+  else {
+    return false
+  }
+}
+
+
+
+
